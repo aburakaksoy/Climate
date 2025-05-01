29fb452f99bf7695dcb4379872acf9ecc16fc698
Burak Aksoy <aburakaksoy@hotmail.com>
2025-05-01 21:31:02 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button"
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Layout from "./components/ui/layout"
+import { ThemeProvider } from "./context/theme-provider"
+import WeatherDashBoard from "./pages/weather-dashboard"
+import CityPage from "./pages/city-page"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      staleTime: 5*60*1000,
+      gcTime: 10*60*1000,
+      retry: false,
+      refetchOnWindowFocus: false,
+    }
+  }
+});
+
+function App() {
+return (
+  <QueryClientProvider client={queryClient}>
+
+   <BrowserRouter>
+    <ThemeProvider defaultTheme="dark">
+      <Layout>
+        <Routes>
+          <Route path="/" element={<WeatherDashBoard/>}/>
+          <Route path="/city/:cityName" element={<CityPage />}/>
+        </Routes>
+      </Layout>
+    </ThemeProvider>  
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>  
+  )
+}
+
+export default App
